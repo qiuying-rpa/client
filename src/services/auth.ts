9a7b21a7572a6eac7e3109d242005f064280472f
陶清秋 <allen@taoqingqiudeMacBook-Air.local>
2023-06-03 11:27:38 +0800

    fix: solve the Permissions type repeat problem

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,7 +2,7 @@ import axios from "@/services"
 
 export const getPublicKey = () => axios.get<any, string>('/static/rsa_pub_key.pem')
 
-export const getPermissions = () => axios.get<any, BaseResponse<Permissions>>('/auth/permissions')
+export const getPermissions = () => axios.get<any, BaseResponse<Qiuying.Permissions>>('/auth/permissions')
 
 export const getToken = (email: string, password: string) => axios.post<any, BaseResponse<GetTokenData>>('/auth/token', { email, password })
 
